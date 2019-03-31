086f09367e12c1a851b4c08c9fa43f3aa00840a2
ando07 <ando.masahito@gmail.com>
2019-03-31 21:43:13 +0900

    try reviewdog

--- sample01.go
+++ sample01.go
@@ -1,7 +1,9 @@
 package sample01
 
 func HelloWorld(s string) string {
-		var y int
-		y = "aaa"
+	var y int
+	y = "aaa" // TODO
 	return "hello world, " + s
 }
+
+var ExportedVarWithoutComment = 1 // TODO: revert
