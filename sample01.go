1af1d7898eb257bd02189793a8227483519a827d
ando07 <ando.masahito@gmail.com>
2019-03-31 21:56:13 +0900

    try reviewdog

--- sample01.go
+++ sample01.go
@@ -1,5 +1,7 @@
 package sample01
 
+import "fmt"
+
 func HelloWorld(s string) string {
 	var y int
 	y = "aaa" // TODO
@@ -7,3 +9,7 @@ func HelloWorld(s string) string {
 }
 
 var ExportedVarWithoutComment = 1 // TODO: revert
+
+func Greet() {
+	fmt.Println("Hello, World!")
+}
