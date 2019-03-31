98546577fa13a2677ad063c01c1b17394020b08d
ando07 <ando.masahito@gmail.com>
2019-03-31 23:08:50 +0900

    sample go vet

--- sample01.go
+++ sample01.go
@@ -2,12 +2,6 @@ package sample01
 
 import "fmt"
 
-type Connpass struct {
-	ResultsReturned  int `json:results_returned`
-	ResultsAvailable int `json:results_available`
-	ResultsStart     int `json:results_start`
-}
-
 func HelloWorld(s string) string {
 	return "hello world, " + s
 }
@@ -17,3 +11,5 @@ var ExportedVarWithoutComment = 1 // TODO: revert
 func Greet() {
 	fmt.Println("Hello, World!")
 }
+
+var vetError = fmt.Sprintf("", 1, 2, 3) // TODO: revert
